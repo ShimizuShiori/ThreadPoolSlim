56ee5a3fd2a6a80b4d4b95df7101dbe5b4de9e4c
Felix Fei <Felix.Fei@wisetechglobal.com>
2023-03-14 20:45:46 +0800

    Tests

--- Src/Reface.ThreadPoolSlim/Reface.ThreadPoolSlim/FixedThreadPoolSlim.cs
+++ Src/Reface.ThreadPoolSlim/Reface.ThreadPoolSlim/FixedThreadPoolSlim.cs
@@ -1,18 +1,23 @@
-﻿namespace Reface.ThreadPoolSlim
+﻿using System.Diagnostics;
+
+namespace Reface.ThreadPoolSlim
 {
 	public class FixedThreadPoolSlim : IThreadPoolSlim
 	{
+		readonly object extThreadLock = new object();
+		readonly object coreThreadLock = new object();
+		readonly AutoResetEvent hasJobsEvent = new AutoResetEvent(false);
+
 		readonly int coreSize;
 		readonly int maxSize;
 		readonly Func<Thread> threadFactory;
 		readonly TimeSpan keepAliveTime;
 		readonly Queue<(Action<object>, object)> jobs;
 		readonly int maxJobSize;
-		readonly ICollection<Thread> coreThread = new List<Thread>();
-		readonly ICollection<Thread> extThread = new List<Thread>();
-		readonly AutoResetEvent hasJobsEvent = new AutoResetEvent(false);
 		readonly Action fullHandler;
 
+		int runningCoreThreadCount = 0;
+		int runningExtThreadCount = 0;
 		int maxId = 0;
 
 		public FixedThreadPoolSlim(
@@ -28,13 +33,12 @@
 			this.maxJobSize = maxJobSize;
 			this.jobs = new Queue<(Action<object>, object)>();
 			this.threadFactory = threadFactory ?? new Func<Thread>(() =>
-			{
-				var id = Interlocked.Increment(ref maxId);
-				var t = new Thread(DoJobs);
-				t.Name = $"worker-{id}";
-				t.Start();
-				return t;
-			});
+			 {
+				 var id = Interlocked.Increment(ref maxId);
+				 var t = new Thread(DoJobs);
+				 t.Name = $"worker-{id}";
+				 return t;
+			 });
 			this.fullHandler = fullHandler ?? new Action(() =>
 			{
 				throw new JobQueueIsFullException();
@@ -44,42 +48,65 @@
 
 		public void Submit(Action<object> action, object args)
 		{
+			Debug.WriteLine($"Args={args}");
 			bool enqueued = false;
 			lock (jobs)
 			{
+				Debug.WriteLine($"jobs.Count={jobs.Count}, maxJobSize={maxJobSize}");
 				if (jobs.Count < this.maxJobSize)
 				{
 					jobs.Enqueue((action, args));
 					hasJobsEvent.Set();
 					enqueued = true;
+					Debug.WriteLine("Queued");
 				}
 			}
-			lock (coreThread)
+			lock (coreThreadLock)
 			{
-				if (coreThread.Count < this.coreSize)
+				Debug.WriteLine($"runningCoreThreadCount={runningCoreThreadCount}, coreSize={coreSize}");
+				if (runningCoreThreadCount < this.coreSize)
 				{
-					var t = threadFactory();
-					coreThread.Add(t);
-					return;
-				}
-			}
-			lock (extThread)
-			{
-				if (extThread.Count < this.maxSize)
-				{
-					var t = threadFactory();
-					extThread.Add(t);
+					Action callback = () =>
+					{
+						lock (coreThreadLock)
+							runningCoreThreadCount--;
+					};
+					threadFactory().Start(callback);
+					runningCoreThreadCount++;
+					Debug.WriteLine("Core Thread Created");
 					return;
 				}
 			}
 			if (enqueued)
 				return;
 
+			lock (jobs)
+				lock (extThreadLock)
+				{
+					Debug.WriteLine($"runningExtThreadCount={runningExtThreadCount}, maxSize={maxSize}");
+					if (runningExtThreadCount < this.maxSize)
+					{
+						Action callback = () =>
+						{
+							lock (extThreadLock)
+								runningExtThreadCount--;
+						};
+						jobs.Enqueue((action, args));
+						var t = threadFactory();
+						t.Name = "ext-" + t.Name;
+						t.Start(callback);
+						runningExtThreadCount++;
+						Debug.WriteLine("Ext Thread Created");
+						return;
+					}
+				}
+			Debug.WriteLine("Calling fullHandler()");
 			fullHandler();
 		}
 
 		void DoJobs(object? obj)
 		{
+			Debug.WriteLine($"[{Thread.CurrentThread.Name}] Started");
 			bool waitForJobs = false;
 			while (true)
 			{
@@ -103,10 +130,14 @@
 				}
 
 
-
+				Debug.WriteLine($"[{Thread.CurrentThread.Name}] Doing Job");
 				job.Item1(job.Item2);
+				Debug.WriteLine($"[{Thread.CurrentThread.Name}] Finished Job");
 				Thread.Sleep(0);
 			}
+			Debug.WriteLine($"[{Thread.CurrentThread.Name}] Dead");
+			if (obj is Action callback)
+				callback();
 		}
 	}
 }
